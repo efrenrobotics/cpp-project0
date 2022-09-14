c3f309e8b9f303056eefe9103d8bb0bf6a593236
Angel Vega Torres <angel729@cs.utexas.edu>
2022-09-13 20:54:01 -0500

    fixed with no global variables

--- spaceX.cpp
+++ spaceX.cpp
@@ -2,8 +2,8 @@
 
 // Displays the items menu
 // Efren
-void displayMenu() {
-    for (int i = 1; i <= 6; i++) {
+void displayMenu(double prices[], string description[]) {
+    for (int i = 1; i <= NUM_ITEMS; i++) {
         cout << "[" << i << "] " << left << setw(19) << description[i - 1];
         cout << setprecision(2) << fixed;
         cout << "$" << right << setw(7) << prices[i-1] << endl;
@@ -13,11 +13,12 @@ void displayMenu() {
 
 // Get user input, print error if item outside of menu
 // Efren
-void getInput() {
+double getSubTotal(double prices[], int totalItemCount[]) {
     int input = -1;
+    int itemCount[NUM_ITEMS] = {0};
     while (input != 0) {
         cin >> input;
-        if (input < 0 || input > 6) {
+        if (input < 0 || input > NUM_ITEMS) {
             cout << "*Error: There is no item " << input << "." << endl;
             cout << "Processing other items..." << endl;
         } else {
@@ -25,59 +26,64 @@ void getInput() {
             totalItemCount[input - 1]++;
         }
     }
+    double total = 0;
+    for (int i = 0; i < NUM_ITEMS; i++) {
+        total += (prices[i] * itemCount[i]);
+    }
+    return total;
 }
 
 // Display subtotal, tax-total, and total
 // Angel
-bool printPrice() {
-    double total = 0;
-    for (int i = 0; i < 6; i++) {
-        total += (prices[i] * itemCount[i]);
-    }
-    cout << "Subtotal...$" << right << setw(8) << total << endl;
-    double tax = total * TAX;
+double printPrice(double subTotal) {
+    cout << "Subtotal...$" << right << setw(8) << subTotal << endl;
+    double tax = subTotal * TAX;
     cout << "Tax........$" <<  right << setw(8) << tax << endl;
-    total += (total * TAX);
-    totalForDay += total;
+    double total = subTotal + (subTotal * TAX);
     cout << "Total......$" << right << setw(8) << total << endl;
     cout << "\nPlease enter 'N' to take a new order and 'E' to exit: ";
-    string playAgain;
-    cin >> playAgain;
-    return playAgain.compare("N") == 0;
+    return total;
 }
 
-// Zero out the array
-// Efren
-void resetCount() {
-    for (int i = 0; i < 6; i++) {
-        itemCount[i] = 0;
+// Find the index of the most ordered item
+// Angel
+int findMostOrderedItem(int totalItemCount[]) {
+    int maxItemIndex = 0;
+    int max = 0;
+    for (int i = 0; i < NUM_ITEMS; i++) {
+        if(totalItemCount[i] > max) {
+            max = totalItemCount[i];
+            maxItemIndex = i;
+        }
     }
+    return maxItemIndex;
 }
 
 // Display totals for the day
 // Angel
-void printTotal() {
+void printTotal(int totalItemCount[], string description[], double totalForDay) {
     cout << "*** End of Day:" << endl;
     cout << "Total sales: $" << totalForDay << endl;
-    int max = 0;
-    int maxItem = 0;
-    for (int i = 0; i < 6; i++) {
-        if(totalItemCount[i] > max) {
-            max = totalItemCount[i];
-            maxItem = i;
-        }
-    }
-    cout << "Item ordered most often: " << description[maxItem] << " was purchased " << totalItemCount[maxItem] << " times." << endl;
+    int maxItemIndex = findMostOrderedItem(totalItemCount);
+    cout << "Item ordered most often: " << description[maxItemIndex]
+        << " was purchased " << totalItemCount[maxItemIndex] << " times." << endl;
 }
 
 int main() {
+    double prices[NUM_ITEMS] = {1099, 1299, 499, 799, 999, 399};
+    string description[NUM_ITEMS] = {"Rolex Watch", "MacBook Pro", "Men's Cufflinks",
+                                "iPad Pro 11-in", "Ladies Ring", "Watch S4"};
+    int totalItemCount[NUM_ITEMS] = {0};
     bool playAgain = true;
+    double totalForDay = 0;
     while (playAgain) {
-        displayMenu();
-        getInput();
-        playAgain = printPrice();
-        resetCount();
+        displayMenu(prices, description);
+        double subtotal = getSubTotal(prices, totalItemCount);
+        totalForDay += printPrice(subtotal);
+        string input;
+        cin >> input;
+        playAgain = (input.compare("N") == 0);
     }
-    printTotal();
+    printTotal(totalItemCount, description, totalForDay);
     return 0;
 }
--- spaceX.h
+++ spaceX.h
@@ -5,20 +5,10 @@
 using namespace std;
 
 const double TAX = 0.0825;
+const int NUM_ITEMS = 6;
 
-
-// number of items in menu
-int itemCount[6];
-// prices of the menu
-double prices[6] = {1099, 1299, 499, 799, 999, 399};
-// description of menu items
-string description[6] = {"Rolex Watch", "MacBook Pro", "Men's Cufflinks", "iPad Pro 11-in", "Ladies Ring", "Watch S4"};
-// total item for day array
-int totalItemCount[6];
-// total amount earned 
-double totalForDay;
-
-void displayMenu();
-void getInput();
-bool printPrice();
-void resetCount();
+void displayMenu(double prices[], string description[]);
+double getSubTotal(double prices[], int totalItemCount[]);
+double printPrice(double subTotal);
+int findMostOrderedItem(int totalItemCount[]);
+void printTotal(int totalItemCount[], string description[], double totalForDay);
